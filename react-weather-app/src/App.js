c82e8e72a48169ae87b2e28cfcceee3ac579c87f
spiritxhx <spiritxhx@gmail.com>
2019-04-25 19:25:05 -0400

    bug fix No.1

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -4,12 +4,20 @@ import {Title} from './components/Title';
 import {Form} from './components/Form';
 import {Weather} from './components/Weather';
 
+const apiKey ='2df8c55703d7bc6faabcf54584e1bd30';
+
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const apiCall = await fetch(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metrics`);
+    const data = await apiCall.json();
+    console.log(data);
+  }
   render(){
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
--- react-weather-app/src/components/Form.js
+++ react-weather-app/src/components/Form.js
@@ -4,7 +4,7 @@ export class  Form extends React.Component{
     render(){
         return (
             <div>
-                <form>
+                <form onSubmit={this.props.getWeather}>
                     <input type="text" name='city' placeholder="City..." />
                     <input type="text" name='country' placeholder="Country..." />
                     <button>Get Weather</button>
