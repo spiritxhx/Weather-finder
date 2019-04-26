9a63dfd0726c50f90c7c1d9fe29389c7ef0bddde
spiritxhx <spiritxhx@gmail.com>
2019-04-26 16:14:49 -0400

    add conditions in display function

--- react-weather-app/src/components/Weather.js
+++ react-weather-app/src/components/Weather.js
@@ -4,11 +4,10 @@ export class  Weather extends React.Component{
     render(){
         return (
             <div>
-                {this.props.city}
-                {this.props.country}
-                {this.props.temperature}
-                {this.props.humidity}
-                {this.props.description}
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
             </div>
         );
     }
